e5a4c2b10569e696ceded8eb4287fd6c50b42a78
vasylpb <vasylpb89@gmail.com>
2022-10-03 16:16:53 +0300

    Routes added

---
+++ src/pages/requests/ContactCoach.vue
@@ -0,0 +1,1 @@
+<template>Contact Coach</template>
---
+++ src/router.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    {
+      path: '/coaches',
+      component: CoachesList,
+    },
+    {
+      path: '/coaches/:id',
+      component: CoachDetails,
+      children: [{ path: 'contact', component: ContactCoach }],
+    },
+    {
+      path: '/register',
+      component: CoachRegistration,
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound,
+    },
+  ],
+});
+
+export default router;
