9b072a4dbb39d0ec822f1c22c7455ccb03831a85
vasylpb <vasylpb89@gmail.com>
2022-10-03 17:07:31 +0300

    vuex setup

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore } from 'vuex';
+import coachesModule from './modules/coaches/index';
+
+const store = createStore({
+  modules: { coaches: coachesModule },
+});
+
+export default store;
